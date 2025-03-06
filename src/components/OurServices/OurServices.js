03e8273727662b9b1ce4181b88d5f648f88802b6
Martin-IJ <martinohez@gmail.com>
2025-03-06 18:04:57 +0100

    Update image file extensions in OurService component

--- src/components/OurServices/OurServices.js
+++ src/components/OurServices/OurServices.js
@@ -11,21 +11,21 @@ export default function OurServices() {
     {
       icon: <FaDumbbell className="text-4xl text-blue-500" />,
       title: "Strength Training",
-      image: "/images/DSC1.jpg",
+      image: "/images/DSC1.JPG",
       description:
         "Build muscle, tone your body, and increase strength with our professional guidance.",
     },
     {
       icon: <FaRunning className="text-4xl text-green-500" />,
       title: "Cardio Workouts",
-      image: "/images/DSC2.jpg",
+      image: "/images/DSC2.JPG",
       description:
         "Boost your stamina with intense, heart-pumping cardio sessions.",
     },
     {
       icon: <FaUserAlt className="text-4xl text-yellow-500" />,
       title: "Personal Training",
-      image: "/images/DSC3.jpg",
+      image: "/images/DSC3.JPG",
       description:
         "Work one-on-one with our certified trainers for personalized fitness plans.",
     },
